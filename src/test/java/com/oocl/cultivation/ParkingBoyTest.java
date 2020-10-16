01c15ce64501c66f87e2f425acca00479fe6b567
encarju <justine.encarnacion@oocl.com>
2020-10-16 14:06:46 +0800

    [ParkingBoy][ENCARJU] feature: Create test case return ticket when park by Parking Boy

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,20 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_parking_ticket_when_parking_given_a_car_to_parking_boy(){
+        //Given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        //When
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+        //Then
+        assertNotNull(parkingTicket);
 
     }
 }
