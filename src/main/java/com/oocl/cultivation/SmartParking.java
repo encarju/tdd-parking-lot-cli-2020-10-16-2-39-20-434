666f74cd170916051e6b167a8494d0160c5b4e86
encarju <justine.encarnacion@oocl.com>
2020-10-19 18:25:46 +0800

    [ParkingBoy][ENCARJU] refactor: Rename higherParkingLot variable to parkingLotWithHigherCapacity

--- src/main/java/com/oocl/cultivation/SmartParking.java
+++ src/main/java/com/oocl/cultivation/SmartParking.java
@@ -6,9 +6,9 @@ public class SmartParking implements ParkingStrategy {
     @Override
     public ParkingLot getParkingLotOutOfList(List<ParkingLot> parkingLotList) {
         ParkingLot parkingLotOutOfList = parkingLotList.stream().
-                reduce((higherParkingLot,
-                        parkingLot)-> higherParkingLot.getAvailableCapacity() >= parkingLot.getAvailableCapacity()
-                        ? higherParkingLot : parkingLot).get();
+                reduce((parkingLotWithHigherCapacity,
+                        parkingLot)-> parkingLotWithHigherCapacity.getAvailableCapacity()
+                        >= parkingLot.getAvailableCapacity() ? parkingLotWithHigherCapacity : parkingLot).get();
         if((parkingLotOutOfList == null && parkingLotList.size()>0)) {
             parkingLotOutOfList = parkingLotList.get(0);
         }
