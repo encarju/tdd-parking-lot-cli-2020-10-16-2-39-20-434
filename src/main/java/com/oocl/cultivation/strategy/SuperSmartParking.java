620f0aa9f4da0570f108ff226dba0ef8c4eb67f8
encarju <justine.encarnacion@oocl.com>
2020-10-20 10:29:07 +0800

    [ParkingBoy][ENCARJU] refactor: Use max method instead of reduce in SuperSmartParking

--- src/main/java/com/oocl/cultivation/strategy/SuperSmartParking.java
+++ src/main/java/com/oocl/cultivation/strategy/SuperSmartParking.java
@@ -4,15 +4,14 @@ import com.oocl.cultivation.carparking.Constants;
 import com.oocl.cultivation.carparking.ParkingLot;
 import com.oocl.cultivation.exceptions.NotEnoughPositionException;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class SuperSmartParking implements ParkingStrategy {
     @Override
     public ParkingLot getParkingLotOutOfList(List<ParkingLot> parkingLotList) {
-        ParkingLot parkingLotOutOfList = parkingLotList.stream().
-                reduce((parkingLotWithHigherCapacity,
-                        parkingLot)-> parkingLotWithHigherCapacity.getCapacityRate() >= parkingLot.getCapacityRate()
-                        ? parkingLotWithHigherCapacity : parkingLot)
+        ParkingLot parkingLotOutOfList = parkingLotList.stream()
+                .max(Comparator.comparingDouble(ParkingLot::getCapacityRate))
                 .orElseThrow(()-> new NotEnoughPositionException(Constants.NOT_ENOUGH_POSITION_MSG));
         return parkingLotOutOfList;
     }
